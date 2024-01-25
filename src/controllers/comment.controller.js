9d1754238b5c638b91c8ae3a3a979031f41d6135
kuntal <km7999319@gmail.com>
2024-01-25 18:32:18 +0530

    Add comment controller functions and update comment model and router

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,167 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
-import { verifyJWT } from '../middlewares/auth.middleware.js';
 import { ApiError } from '../utils/ApiError.js';
 import { ApiResponce } from '../utils/ApiResponce.js';
 import mongoose from 'mongoose';
+import {Comment} from "../models/comments.model.js";
+
+const createComment = asyncHandler(async (req, res) => {
+    const {text,web} = req.body;
+
+    if (!text || !web) {
+        throw new ApiError(400, 'Text and web both are required');
+    }
+
+    const comment = await Comment.create({
+        text:text,
+        web:new mongoose.Types.ObjectId(web),
+        owner:new mongoose.Types.ObjectId(req.user?._id)
+    });
+
+    if (!comment) {
+        throw new ApiError(500, 'Something went wrong while creating comment');
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponce(200,comment, 'Comment created successfully'));
+})
+
+
+const updateComment = asyncHandler(async (req, res) => {
+    const {text,commentId} = req.body;
+
+    if (!commentId) {
+        throw new ApiError(400, 'Comment id is required');
+    }
+
+    if (!text) {
+        throw new ApiError(400, 'Text is required');
+    }
+
+    const comment = await Comment.findByIdAndUpdate(commentId,{
+        text:text
+    },{
+        new:true
+    });
+
+    if (!comment) {
+        throw new ApiError(500, 'Something went wrong while updating comment');
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponce(200,comment, 'Comment updated successfully'));
+});
+
+
+const deleteComment = asyncHandler(async (req, res) => {
+    const {commentId} = req.params;
+
+    if (!commentId) {
+        throw new ApiError(400, 'Comment id is required');
+    }
+
+    const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+    if (!deletedComment) {
+        throw new ApiError(500, 'Something went wrong while deleting comment');
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponce(200,{}, 'Comment deleted successfully'));
+});
+
+
+const getAllWebComments = asyncHandler(async (req, res) => {
+    const {webId,page=1,limit=20} = req.query;
+
+    if (!webId || !mongoose.isValidObjectId(webId)) {
+        throw new ApiError(400, 'Web id is required');
+    }
+
+    const comments = await Comment.aggregatePaginate([
+        {
+            $match:{
+                web:new mongoose.Types.ObjectId(webId),
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"owner",
+                pipeline:[
+                    {
+                        $project:{
+                            username:1,
+                            fullName:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $lookup:{
+                from:"likes",
+                localField:"_id",
+                foreignField:"comment",
+                as:"likes"
+            }
+        },
+        {
+            $lookup:{
+                from:"replays",
+                localField:"_id",
+                foreignField:"comment",
+                as:"replays"
+            }
+        },
+        {
+            $addFields:{
+                likesCount:{
+                    $size:"$likes"
+                },
+                replaysCount:{
+                    $size:"$replays"
+                },
+                owner:{
+                    $first:"$owner"
+                },
+                isLikedByMe:{
+                    $cond:{
+                        if:{
+                            $in:[new mongoose.Types.ObjectId(req.user?._id),"$likes.likedBy"]
+                        },
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                likes:0,
+                replays:0
+            }
+        },
+        {
+            $sort:{
+                createdAt:-1
+            }
+        }
+    ],{
+        limit:parseInt(limit),
+        page:parseInt(page)
+    });
+
+})
+
+export {
+    createComment,
+    updateComment,
+    deleteComment,
+    getAllWebComments
+}
--- src/routes/comments.router.js
+++ src/routes/comments.router.js
@@ -1,7 +1,20 @@
 import { Router } from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
+import {
+    createComment,
+    updateComment,
+    deleteComment,
+    getAllWebComments
+} from "../controllers/comment.controller.js"
 
 const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/create").post(createComment);
+router.route("/update").patch(updateComment);
+router.route("/delete/:commentId").delete(deleteComment);
+router.route("/get-comments").get(getAllWebComments);
 
 
 
