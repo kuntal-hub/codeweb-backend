24896f0d20c6e27301306cc741ecd1ac7f96d278
kuntal <km7999319@gmail.com>
2024-04-02 13:35:35 +0530

    Fix token check in auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,7 +9,7 @@ const verifyJWT = asyncHandler(async(req,res,next)=>{
         // get token from header or cookie
         const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
         // check if token exists
-        if (!token) {
+        if (!token || token === "null") {
             throw new ApiError(403,"Unauthorized request");
         }
         // verify token
